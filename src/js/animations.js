75298c0dc37db77ef2e59fd572d5a7ad2d1a890d
Christofer Hansson <christofer_hansson@hotmail.com>
2025-02-25 19:38:45 +0100

    Har gjort en chatbot!!!

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,22 +1,105 @@
 "use strict";
 
 let impossibleEl = document.getElementById("impossiblebutton");
+let answerEl = document.getElementById("answer");
+let inputEl = document.getElementById("who");
+let tackEl = document.getElementById("tackbutton");
+let answer2El = document.getElementById("answer2");
+let njaaEl = document.getElementById("njaabutton");
+let answer3El = document.getElementById("answer3");
+let okeeejEl = document.getElementById("okeeejbutton");
+let lookalikeEl = document.getElementById("lookalike");
+let reactionEl = document.getElementById("reactionbuttons");
+let reaction1El = document.getElementById("reaction1button");
+let reaction2El = document.getElementById("reaction2button");
+let answer4El = document.getElementById("answer4");
+
 
 impossibleEl.addEventListener("mouseover", moveButton);
 impossibleEl.addEventListener("mouseout", stayButton);
 
+inputEl.addEventListener("keypress", function(name) {
+    if (name.key === "Enter") {
+
+        answerText();
+    }
+});
+
+
 
 function moveButton () {
-    console.log("working...");
-
     impossibleEl.style.animationPlayState = "running";
-
 }
 
 function stayButton () {
-    console.log("also working...");
-
     impossibleEl.style.animationPlayState = "paused";
     impossibleEl.style.animationDelay = "100ms";
+}
 
+function answerText() {
+    let inputName = inputEl.value.toLowerCase();
+
+    answerEl.innerHTML ="";
+
+    if (inputName === "mattias") {
+        answerEl.innerHTML = "Hej Mattias! Vad kul att det är du som rättar denna uppgift.";
+        tackEl.style.display = "flex";
+
+    } else if (inputName === "malin") {
+        answerEl.innerHTML = "Hej Malin! Vad kul att det är du som rättar denna uppgift.";
+        tackEl.style.display = "flex";
+
+    } else {
+        answerEl.innerHTML = "Oj, vem är du och hur har du hittat hit?";
+    }
+
+    tackEl.addEventListener("click", function() {
+        answerText2(inputName);
+    });
+
+}
+
+function answerText2(inputName) {
+
+    if (inputName === "mattias") {
+        answer2El.innerHTML = "Det hade gått bra med Malin också såklart. Ni är ganska lika trots allt. Har du tänkt på det?";
+        njaaEl.style.display = "flex";
+
+    } else if (inputName === "malin") {
+        answer2El.innerHTML = "Det hade gått bra med Mattias också såklart. Ni är ganska lika trots allt. Har du tänkt på det?";
+        njaaEl.style.display = "flex";
+    }
+
+
+    njaaEl.addEventListener("click", function() {
+        answerText3();
+    });
+}
+
+function answerText3() {
+
+
+    answer3El.innerHTML = "Jo kolla här. Är egentligen bara håret och glasögoen som skiljer!";
+    okeeejEl.style.display = "flex";
+
+    okeeejEl.addEventListener("click", showLookalike);
+}
+
+
+function showLookalike() {
+    lookalikeEl.style.display = "flex";
+    reactionEl.style.display = "flex";
+
+    reaction1El.addEventListener("click", answerText4happy);
+    reaction2El.addEventListener("click", answerText4mad);
+}
+
+function answerText4happy() {
+    answer4El.innerHTML = "Tjoho! Tack, ni är bäst!";
+
+}
+
+function answerText4mad() {
+    answer4El.innerHTML = "Förlåt...det var bara på skoj :( Ni är inte alls lika!";
+ 
 }
