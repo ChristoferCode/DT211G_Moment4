616833f02b500d86cbdf33bc4cc9b65854fbe1a7
Christofer Hansson <christofer_hansson@hotmail.com>
2025-02-25 10:57:16 +0100

    lite mer animeringstest

---
+++ src/js/animations.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let impossibleEl = document.getElementById("impossiblebutton");
+
+impossibleEl.addEventListener("mouseover", moveButton);
+impossibleEl.addEventListener("mouseout", stayButton);
+
+
+function moveButton () {
+    console.log("working...");
+
+    impossibleEl.style.animationPlayState = "running";
+
+}
+
+function stayButton () {
+    console.log("also working...");
+
+    impossibleEl.style.animationPlayState = "paused";
+}
